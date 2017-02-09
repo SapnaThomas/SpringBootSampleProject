5ba8b98d221a3219aab491ede4a794f736b754d3
Sumi Cherian <Sumi_Cherian@ad.infosys.com>
2017-02-09 17:46:18 +0530

    commit

--- src/main/java/com/test/Start.java
+++ src/main/java/com/test/Start.java
@@ -27,14 +27,7 @@ public class Start {
 	WebSocketClient webSocketClient;
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Start.class, args);
-/*        String uri = "ws://localhost:8181/websocket";
-        System.out.println("Connecting to " + uri);
-        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-        try {
-        container.connectToServer(WebSocketClient.class, URI.create(uri));
-        } catch (DeploymentException | IOException e) {
-            e.printStackTrace();
-        }*/
+
 
 	}
 
